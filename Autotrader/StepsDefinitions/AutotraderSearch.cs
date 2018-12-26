fde72c0232ce9fd4d0aee2650b80d159c12a823f
opechika <opechika@yahoo.com>
2018-12-26 21:42:54 +0000

    Added Webdriver

--- Autotrader/StepsDefinitions/AutotraderSearch.cs
+++ Autotrader/StepsDefinitions/AutotraderSearch.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,15 @@ using TechTalk.SpecFlow;
 
 namespace Autotrader.StepsDefinitions
 {
+    
+
     [Binding]
     public sealed class AutotraderSearch
     {
+        IWebDriver driver;
+
+       
+
         [Given(@"I am on Autotrader homepage")]
         public void GivenIAmOnAutotraderHomepage()
         {
