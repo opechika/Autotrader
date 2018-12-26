b029a9f7834fdf73f06c4a015bdcbe0ab623222f
opechika <opechika@yahoo.com>
2018-12-26 21:23:36 +0000

    First Automation Commit

---
+++ Autotrader/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Autotrader")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Autotrader")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6c2dc6be-836c-4101-9497-74cc8dfef298")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Autotrader/StepsDefinitions/AutotraderSearch.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace Autotrader.StepsDefinitions
+{
+    [Binding]
+    public sealed class AutotraderSearch
+    {
+        [Given(@"I am on Autotrader homepage")]
+        public void GivenIAmOnAutotraderHomepage()
+        {
+            
+        }
+
+        [When(@"I enter postcode as ""(.*)""")]
+        public void WhenIEnterPostcodeAs(string postcode)
+        {
+            
+        }
+
+        [When(@"I select distance from Postcode as ""(.*)""")]
+        public void WhenISelectDistanceFromPostcodeAs(string distance)
+        {
+            
+        }
+
+        [When(@"I select the Car Make as ""(.*)""")]
+        public void WhenISelectTheCarMakeAs(string make)
+        {
+            
+        }
+
+        [When(@"I select the Car Model as ""(.*)""")]
+        public void WhenISelectTheCarModelAs(string model)
+        {
+            
+        }
+
+        [Then(@"I click on the search for car button")]
+        public void ThenIClickOnTheSearchForCarButton()
+        {
+            
+        }
+
+        [Then(@"the result of car ""(.*)"" is displayed")]
+        public void ThenTheResultOfCarIsDisplayed(string result)
+        {
+            
+        }
+
+    }
+}
