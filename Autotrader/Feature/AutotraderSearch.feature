7aa9c9dc788f06177b981e82f4e17412b34810a3
opechika <opechika@yahoo.com>
2018-12-31 00:28:17 +0000

    Committing New Scenarios

---
+++ Autotrader/Feature/AutotraderSearch.feature
@@ -0,0 +1,18 @@
+﻿Feature: AutotraderSearch
+	In order to buy a car of my choice 
+	As a customer
+	I want to search for a car of my choice
+
+Scenario Outline: Search for car of my choice
+	Given I am on Autotrader homepage
+	When I enter postcode as "<Postcode>"
+	And I select distance from Postcode as "<Distance>"
+	And I select the Car Make as "<Make>"
+	And I select the Car Model as "<Model>"
+	Then I click on the search for car button
+	Then the result of car "<Make>" is displayed
+
+Scenarios: 
+| Postcode | Distance        | Make   | Model   |
+| OL66BJ   | Within 10 miles | Audi   | A3      |
+| M345JA   | Within 30 miles | Nissan | Qashqai |
---
+++ Autotrader/StepDefinitions/AutotraderSearchSteps.cs
@@ -0,0 +1,88 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace Autotrader.StepDefinitions
+{
+    [Binding]
+    public sealed class AutotraderSearchSteps
+    {
+        IWebDriver driver;
+        IWebElement Code;
+        IWebElement DistanceFromPostcode;
+        IWebElement CarMake;
+        IWebElement CarModel;
+        IWebElement SearchButton;
+        IWebElement CookiesBar;
+
+
+
+        [Given(@"I am on Autotrader homepage")]
+        public void GivenIAmOnAutotraderHomepage()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.autotrader.co.uk/");
+            driver.Manage().Window.Maximize();
+        }
+
+        [When(@"I enter postcode as ""(.*)""")]
+        public void WhenIEnterPostcodeAs(string pcode)
+        {
+            Code = driver.FindElement(By.Id("postcode"));
+            Code.SendKeys(pcode);
+        }
+
+        [When(@"I select distance from Postcode as ""(.*)""")]
+        public void WhenISelectDistanceFromPostcodeAs(string distance)
+        {
+            DistanceFromPostcode = driver.FindElement(By.Id("radius"));
+            SelectElement select = new SelectElement(DistanceFromPostcode);
+            select.SelectByText(distance);
+            
+        }
+
+        [When(@"I select the Car Make as ""(.*)""")]
+        public void WhenISelectTheCarMakeAs(string make)
+        {
+            Thread.Sleep(5000);
+            CarMake = driver.FindElement(By.Id("searchVehiclesMake"));
+            SelectElement select = new SelectElement(CarMake);
+            select.SelectByValue(make.ToUpper());
+          
+        }
+
+        [When(@"I select the Car Model as ""(.*)""")]
+        public void WhenISelectTheCarModelAs(string model)
+        {
+            Thread.Sleep(5000);
+            CarModel = driver.FindElement(By.Id("searchVehiclesModel"));
+            SelectElement select = new SelectElement(CarModel);
+            select.SelectByValue(model.ToUpper());
+
+          
+
+        }
+
+        [Then(@"I click on the search for car button")]
+        public void ThenIClickOnTheSearchForCarButton()
+        {
+            CookiesBar = driver.FindElement(By.Id("js-cookie-alert-close"));
+            CookiesBar.Click();
+            Thread.Sleep(3000);
+            SearchButton = driver.FindElement(By.Id("js-search-button"));
+            SearchButton.Click();
+        }
+
+        [Then(@"the result of car ""(.*)"" is displayed")]
+        public void ThenTheResultOfCarIsDisplayed(string result)
+        {
+
+        }
+    }
+}
